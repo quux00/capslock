9f4bdc4cd9fcd5934e866b0e9345e4dfc734bb69
Michael <mpeterson2@gmail.com>
2012-07-29 16:41:02 -0400

    capslock2 (functions) working with first iteration function API - callbacks.

--- capslock2.js
+++ capslock2.js
@@ -1,6 +1,23 @@
 (function() {
 
   /* ---[ Helper Functions ]--- */
+  Function.prototype.partial = function() {
+    var fn = this,
+        args = Array.prototype.slice.call(arguments);
+  
+    return function() {
+      var arg = 0,
+          iargs = args.slice();
+      for (var i = 0; i < args.length && arg < arguments.length; i++) {
+        if (args[i] === undefined) {
+          iargs[i] = arguments[arg++];
+        }
+      }
+      return fn.apply(this, iargs.concat( Array.prototype.slice.call(arguments, arg) ));
+    };
+  };
+
+
   function byId(id) {
     return document.getElementById(id);
   }
@@ -11,9 +28,9 @@
    * @param handler - JS callback function when event occurs
    */
   function bindEvent(element, type, handle) {
-    if (element.addEventListener) { 
+    if (element.addEventListener) {
       element.addEventListener(type, handle, false);
-      
+
     } else if (element.attachEvent) {
       element.attachEvent("on" + type, handle);
     }
@@ -22,44 +39,54 @@
   function addClass(elem, cname) {
     elem.className += " " + cname;
   }
-  
+
   function removeClass(elem, cname) {
     var re = new RegExp("(?:^|\\s)" + cname + "(?!\\S)", 'g');
     elem.className = elem.className.replace(re, '');
   }
-  
+
+
   /* ---[ Main Body ]--- */
-  
+
   var warningOn = true;
   var warningElem = byId("warning");
   capsLockCore.reset();
   console.log("...." + capsLockCore.isCapsLockOn());
 
+  // coupled
   function capsLockOn() {
     removeClass(warningElem, "hidden");
     addClass(warningElem, "visible")
   }
-  
+
+  // coupled
   function capsLockOff() {
     removeClass(warningElem, "visible");
     addClass(warningElem, "hidden")
   }
-  
+
+  // coupled
   function handler(e) {
+    checkCapsLock(e, capsLockOn, capsLockOff);
+  }
+
+  var h2 = checkCapsLock.partial(undefined, capsLockOn, capsLockOff);
+  
+  // generic
+  function checkCapsLock(e, fnOn, fnOff) {
     var state = capsLockCore.analyzeEvent(e);
     if (state.changed) {
-      if (state.on) capsLockOn();
-      else          capsLockOff();
+      state.on ? fnOn() : fnOff();
     }
   }
-  
+
   var login = byId("login-field");
   var passw = byId("password-field");
 
-  bindEvent(login, "keypress", handler);
-  bindEvent(passw, "keypress", handler);
-  bindEvent(login, "keydown", handler);
-  bindEvent(passw, "keydown", handler);
+  bindEvent(login, "keypress", h2);
+  bindEvent(passw, "keypress", h2);
+  bindEvent(login, "keydown", h2);
+  bindEvent(passw, "keydown", h2);
 })();
 
 
